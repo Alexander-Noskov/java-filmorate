2f90c44ce59e02d26b9c4dad58d42aad96de0052
Alexander Noskov <alexaner199513@yandex.ru>
2025-02-03 13:10:01 +0600

    Изменить тесты для слоя хранения данных

---
+++ src/test/java/ru/yandex/practicum/filmorate/storage/FilmStorageTest.java
@@ -0,0 +1,89 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class FilmStorageTest {
+    @Autowired
+    private FilmStorage filmStorage;
+
+    @Test
+    void filmCreateTest() {
+        Film filmCreate = Film.builder()
+                .name("Film Name")
+                .description("Film Description")
+                .releaseDate(LocalDate.of(1967, 3, 25))
+                .duration(100L)
+                .build();
+        assertEquals("Film Name", filmStorage.addFilm(filmCreate).getName());
+    }
+
+    @Test
+    void filmUpdateTest() {
+        Film filmCreate = Film.builder()
+                .name("Film Name")
+                .description("Film Description")
+                .releaseDate(LocalDate.of(1967, 3, 25))
+                .duration(100L)
+                .build();
+        Film filmUpdate = filmStorage.addFilm(filmCreate);
+        filmUpdate.setName("Film Update");
+        assertEquals("Film Update", filmStorage.updateFilm(filmUpdate).getName());
+    }
+
+    @Test
+    void filmUpdateUnknownTest() {
+        Film filmUpdateUnknown = Film.builder()
+                .id(999L)
+                .name("Film Name")
+                .description("Film Description")
+                .releaseDate(LocalDate.of(1967, 3, 25))
+                .duration(100L)
+                .build();
+        assertThrows(NotFoundException.class, () -> filmStorage.updateFilm(filmUpdateUnknown));
+    }
+
+    @Test
+    void filmGetAllTest() {
+        Film film = Film.builder()
+                .name("Film Name")
+                .description("Film Description")
+                .releaseDate(LocalDate.of(1967, 3, 25))
+                .duration(100L)
+                .build();
+        Film filmCreated = filmStorage.addFilm(film);
+
+        List<Film> filmGetAll = filmStorage.getAllFilms();
+        assertTrue(filmGetAll.contains(filmCreated));
+    }
+
+    @Test
+    void filmGetByIdTest() {
+        Film film = Film.builder()
+                .name("Film Name")
+                .description("Film Description")
+                .releaseDate(LocalDate.of(1967, 3, 25))
+                .duration(100L)
+                .build();
+        Film filmCreated = filmStorage.addFilm(film);
+        assertEquals("Film Name", filmStorage.getFilmById(filmCreated.getId()).getName());
+    }
+
+    @Test
+    void filmGetFilmByIdUnknownTest() {
+        assertThrows(NotFoundException.class, () -> filmStorage.getFilmById(999L));
+    }
+}
